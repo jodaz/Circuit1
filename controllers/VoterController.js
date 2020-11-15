7dc17e79bf71b616f66fe1d52c88a3c0cdc8cb92
Jesús Ordosgoitty <jesuodz@gmail.com>
2020-11-15 16:49:06 -0400

    Custom filter

--- controllers/VoterController.js
+++ controllers/VoterController.js
@@ -1,13 +1,13 @@
 const Model = require('../models/Person');
 
 const get = async (req, res) =>{ 
-  const { page, perPage } = req.query;
+  const { page, perPage, filter } = req.query;
 
   const limit = parseInt(perPage);
   const skip = (page == 1) ? 0 : page * perPage - perPage;
   const total = await Model.count({});
 
-  await Model.find()
+  await Model.find(query)
     .limit(limit) 
     .skip(skip)
     .sort({ createdAt: -1 })
