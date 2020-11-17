cbef1dd9c81468ea18c1172802bf3c6ffac28304
Jesus Ordosgoitty <jesuodz@gmail.com>
2020-11-17 07:14:53 -0400

    Refactor arquitecture

--- app.js
+++ app.js
@@ -28,9 +28,9 @@ db.start();
 require('./routes')(app);
 
 if (APP_ENV = 'production') {
-  app.use(express.static('client/build'));
+  app.use(express.static('build'));
   app.get('*', (req, res) => {
-    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+    res.sendFile(path.resolve(__dirname, 'build', 'index.html'));
   });
 } 
 
